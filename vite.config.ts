a7310b823158ac44eff7b1bfd62152cb7fb35c44
EdmundMartin <edmartin101@gmail.com>
2024-12-07 09:51:01 +0000

    Fix build

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from 'tailwindcss'
-import legacy from "@vitejs/plugin-legacy";
-
-const legacyPluginOptions = {
-  modernTargets: "since 2023-01-01, not dead",
-  modernPolyfills: true,
-  renderLegacyChunks: false,
-} as const;
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), legacy(legacyPluginOptions)],
+  plugins: [react()],
   css: {
     postcss: {
       plugins: [tailwindcss()],
